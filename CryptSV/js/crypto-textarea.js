ff7ad0a26f82184d813657201cb4c1658a58bc65
Julian Clot <JulianClot@Julians-MacBook-Pro.local>
2020-12-07 15:47:57 -0600

    create a js crypto file

---
+++ CryptSV/js/crypto-textarea.js
@@ -0,0 +1,16 @@
+function encrypt(textarea) {
+
+    
+
+
+}
+
+
+
+function valueTextArea() {
+
+    const textarea = document.getElementById('decrypt-form').value;
+    encrypt(textarea);
+    document.getElementById("text-decrypted").innerHTML = textarea;
+
+}
